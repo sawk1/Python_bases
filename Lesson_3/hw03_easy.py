a69ec1e9d1c4df1aa52351d9e41a6166017be07b
sawk <sanchosos@yandex.ru>
2019-02-01 00:00:52 +0300

    1

--- Lesson_3/hw03_easy.py
+++ Lesson_3/hw03_easy.py
@@ -1,24 +1,25 @@
-# Задание-1:
-# Напишите функцию, округляющую полученное произвольное десятичное число
-# до кол-ва знаков (кол-во знаков передается вторым аргументом).
-# Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
-# Для решения задачи не используйте встроенные функции и функции из модуля math.
+'''
+import random
+lst = [random.randint(-10,10) for i in range(10)]
+for i in lst[:]:
+    if i<0:
+        lst.remove(i)
+print (lst)
 
+<<<<<<< Updated upstream
 def udalenie(n,spisok):
     for i in range(len(spisok)):
         if len(spisok)!= n:
             spisok.pop() #удаляем все элементы после n
+=======
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+>>>>>>> Stashed changes
 
-def okruglenie(b,n,spisok):
-    for i in b[2:len(b)]:
-        k = int(i)
-        spisok.append(k)
-    if spisok[n] > 5:
-        spisok[n - 1] = spisok[n - 1] + 1
-        udalenie(n,spisok)
-    else:
-        udalenie(n,spisok)
+print (matrix)
 
+<<<<<<< Updated upstream
 def round_chislo(n):
     import random
     a = random.random()
@@ -34,31 +35,59 @@ def round_chislo(n):
 
 n = int(input('кол-во знаков после которых будет округление: '))
 round_chislo(n)
+=======
+for line in matrix:
+    print (line)
+    
+i=0
+while len(matrix)>i:
+    line = matrix[i]
+    j=0
+    while len(line)>j:
+        print ('matrix[{}][{}] = {}'.format(i,j,matrix[i][j]))
+        j+=1
+    i+=1
+>>>>>>> Stashed changes
 
 
-# Задание-2:
-# Дан шестизначный номер билета. Определить, является ли билет счастливым.
-# Решение реализовать в виде функции.
-# Билет считается счастливым, если сумма его первых и последних цифр равны.
-# !!!P.S.: функция не должна НИЧЕГО print'ить
+for i ,line in enumerate(matrix):
+    for j,el in enumerate(line):
+        print ('matrix[{}][{}] = {}'.format(i, j, matrix[i][j]))
 
-def udacha(bilet):
-    bilet1 = []
-    for i in bilet:
-        i = int(i)
-        bilet1.append(i)
-    a = sum(bilet1[0:3])
-    b = sum(bilet1[3:6])
-    if a == b:
-        c = 1
-    else:
-        c = 0
-    return c
-bilet = input('введите 6-значный номер билета: ')
-if udacha(bilet) == 1:
-    print ('вам повезло!')
+print (9 and 5 or 0)
+
+d = {'name':'имя'}
+if d.get('name'):
+    name = d['name']
 else:
-    print ('не повезло')
+    name = 'безымянный'
+print (name)
+
+print (d.get('name') if d.get('name') else 'безымянный')
 
 
+import random
+lst=[]
+for _ in range(10):
+    lst.append(random.randint(-10,10))
+print (lst)
 
+lst_g = [random.randint(-10,10) for _ in range(10)]
+print (lst_g)
+
+lst_i =[input('введите элементы матрицы построчно: ').split() for _ in range(3)]
+for i ,line in enumerate(lst_i):
+    for j,el in enumerate(line):
+        lst_i[i][j]=int(lst_i[i][j])
+for line in lst_i:
+    print (line)
+
+'''
+import re
+
+string = 'this is a simple test message for test'
+pattern1 = '^test'
+
+print (re.match(pattern1,string) is None)
+found = re.findall(r'test', string)
+print (found)
